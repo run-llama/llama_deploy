a562d7ca8596f4fbeb2bd1ed880d82549298272b
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-06-06 15:18:57 -0400

    MessageQueue items (#1)
    * scaffold
    
    * fmt
    
    * wip
    
    * add simple
    
    * test for simple mq
    
    * add different consumer type to test
    
    * deregister test
    
    * add publish test
    
    * infinite loop for passing messages
    
    * lock to privateattr
    
    * minor fixes
    
    ---------
    
    Co-authored-by: Logan Markewich <logan.markewich@live.com>

---
+++ agentfile/message_queues/base.py
@@ -0,0 +1,50 @@
+"""Message queue module."""
+
+from abc import ABC, abstractmethod
+from typing import Any, List, Protocol, Type, TYPE_CHECKING
+from llama_index.core.bridge.pydantic import BaseModel
+from agentfile.messages.base import BaseMessage
+
+if TYPE_CHECKING:
+    from agentfile.message_consumers.base import BaseMessageQueueConsumer
+
+
+class MessageProcessor(Protocol):
+    """Protocol for a callable that processes messages."""
+
+    def __call__(self, message: BaseMessage, **kwargs: Any) -> None:
+        ...
+
+
+class BaseMessageQueue(BaseModel, ABC):
+    """Message broker interface between publisher and consumer."""
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @abstractmethod
+    async def _publish(self, message: BaseMessage, **kwargs: Any) -> Any:
+        """Subclasses implement publish logic here."""
+        ...
+
+    async def publish(self, message: BaseMessage, **kwargs: Any) -> Any:
+        """Send message to a consumer."""
+        await self._publish(message, **kwargs)
+
+    @abstractmethod
+    async def register_consumer(
+        self, consumer: "BaseMessageQueueConsumer", **kwargs: Any
+    ) -> Any:
+        """Register consumer to start consuming messages."""
+
+    @abstractmethod
+    async def deregister_consumer(self, consumer: "BaseMessageQueueConsumer") -> Any:
+        """Deregister consumer to stop publishing messages)."""
+
+    async def get_consumers(
+        self, message_type: Type[BaseMessage]
+    ) -> List["BaseMessageQueueConsumer"]:
+        """Gets list of consumers according to a message type."""
+        raise NotImplementedError(
+            "`get_consumers()` is not implemented for this class."
+        )
---
+++ agentfile/messages/base.py
@@ -0,0 +1,18 @@
+"""Base Message."""
+
+import uuid
+from typing import Any, Optional
+from llama_index.core.bridge.pydantic import BaseModel, Field
+
+
+class BaseMessage(BaseModel):
+    id_: str = Field(default_factory=lambda: str(uuid.uuid4))
+    data: Optional[Any] = Field(default_factory=None)
+
+    @classmethod
+    def class_name(cls) -> str:
+        """Class name."""
+        return "BaseMessage"
+
+    class Config:
+        arbitrary_types_allowed = True
