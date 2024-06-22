4c2d14c6fc4811fca17ead321c32ca3f77b4e79b
Andrei Fajardo <andrei@nerdai.io>
2024-06-22 19:26:31 -0400

    wip

--- agentfile/message_queues/remote.py
+++ agentfile/message_queues/remote.py
@@ -14,23 +14,40 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class RemoteMessageQueue(BaseMessageQueue):
-    url: str
+    base_url: str
     client_kwargs: Optional[Dict] = None
     client: Optional[httpx.AsyncClient] = None
 
     async def _publish(self, message: QueueMessage, **kwargs: Any) -> Any:
-        ...
+        client_kwargs = self.client_kwargs or {}
+        client = self.client or httpx.AsyncClient(**client_kwargs)
+        url = f"{self.base_url}/publish"
+        async with httpx.AsyncClient() as client:
+            await client.post(url, json=message.model_dump())
 
     async def register_consumer(
         self, consumer: BaseMessageQueueConsumer, **kwargs: Any
     ) -> Any:
-        ...
+        client_kwargs = self.client_kwargs or {}
+        client = self.client or httpx.AsyncClient(**client_kwargs)
+        url = f"{self.base_url}/register_consumer"
+        async with httpx.AsyncClient() as client:
+            await client.post(url, json=consumer.model_dump())
 
     async def deregister_consumer(self, consumer: BaseMessageQueueConsumer) -> Any:
-        ...
+        client_kwargs = self.client_kwargs or {}
+        client = self.client or httpx.AsyncClient(**client_kwargs)
+        url = f"{self.base_url}/deregister_consumer"
+        async with httpx.AsyncClient() as client:
+            await client.post(url, json=consumer.model_dump())
 
     async def get_consumers(self, message_type: str) -> List[BaseMessageQueueConsumer]:
-        return []
+        client_kwargs = self.client_kwargs or {}
+        client = self.client or httpx.AsyncClient(**client_kwargs)
+        url = f"{self.base_url}/get_consumers"
+        async with httpx.AsyncClient() as client:
+            res = await client.post(url, json={"message_type": message_type})
+        return res
 
     async def processing_loop(self) -> None:
         pass
@@ -39,4 +56,4 @@ class RemoteMessageQueue(BaseMessageQueue):
         pass
 
     def launch_server(self) -> None:
-        ...
+        pass
