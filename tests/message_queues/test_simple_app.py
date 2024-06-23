0ba0c916ef5120c489bd80a99b0d37a4e33c94d5
Andrei Fajardo <andrei@nerdai.io>
2024-06-22 21:59:01 -0400

    unit test for deregister_consumer

--- tests/message_queues/test_simple_app.py
+++ tests/message_queues/test_simple_app.py
@@ -21,3 +21,23 @@ def test_register_consumer() -> None:
     # assert
     assert response.status_code == 200
     assert response.json() == {"consumer": remote_consumer_def.id_}
+    assert len(mq.consumers) == 1
+
+
+def test_deregister_consumer() -> None:
+    # arrange
+    mq = SimpleMessageQueue()
+    remote_consumer_def = RemoteMessageConsumerDef(
+        message_type="mock_type", url="https://mock-url.io"
+    )
+    test_client = TestClient(mq._app)
+
+    # act
+    _ = test_client.post("/register_consumer", json=remote_consumer_def.model_dump())
+    response = test_client.post(
+        "/deregister_consumer", json=remote_consumer_def.model_dump()
+    )
+
+    # assert
+    assert response.status_code == 200
+    assert len(mq.consumers) == 0
