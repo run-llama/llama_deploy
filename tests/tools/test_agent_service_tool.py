75b9cb6f4b40b519b96b475cab5f0e284d177802
Andrei Fajardo <andrei@nerdai.io>
2024-06-24 12:59:48 -0400

    add final unit test

--- tests/tools/test_agent_service_tool.py
+++ tests/tools/test_agent_service_tool.py
@@ -164,7 +164,7 @@ async def test_tool_call_output(
 @pytest.mark.asyncio()
 @patch.object(ReActAgent, "arun_step")
 @patch.object(ReActAgent, "get_completed_tasks")
-async def test_tool_call_raise_timeout(
+async def test_tool_call_raises_timeout_error(
     mock_get_completed_tasks: MagicMock,
     mock_arun_step: AsyncMock,
     message_queue: SimpleMessageQueue,
@@ -211,7 +211,56 @@ async def test_tool_call_raise_timeout(
 
 
 @pytest.mark.asyncio()
-async def test_tool_call_hits_timeout_returns_tool_output(
-    message_queue: SimpleMessageQueue, agent_service: AgentService
+@patch.object(ReActAgent, "arun_step")
+@patch.object(ReActAgent, "get_completed_tasks")
+async def test_tool_call_hits_timeout_but_returns_tool_output(
+    mock_get_completed_tasks: MagicMock,
+    mock_arun_step: AsyncMock,
+    message_queue: SimpleMessageQueue,
+    agent_service: AgentService,
+    task_step_output: TaskStepOutput,
+    completed_task: Task,
 ) -> None:
-    ...
+    # arrange
+    def arun_side_effect(task_id: str) -> TaskStepOutput:
+        completed_task.task_id = task_id
+        task_step_output.task_step.task_id = task_id
+        return task_step_output
+
+    mock_arun_step.side_effect = arun_side_effect
+    mock_get_completed_tasks.side_effect = [
+        [],
+        [],
+        [completed_task],
+        [completed_task],
+        [completed_task],
+    ]
+
+    agent_service_tool = AgentServiceTool.from_service_definition(
+        message_queue=message_queue,
+        service_definition=agent_service.service_definition,
+        timeout=1e-12,
+        raise_timeout=False,
+    )
+
+    # startup
+    await message_queue.register_consumer(agent_service.as_consumer())
+    mq_task = asyncio.create_task(message_queue.processing_loop())
+    as_task = asyncio.create_task(agent_service.processing_loop())
+
+    # act/assert
+    tool_output = await agent_service_tool.acall(input="What is the secret fact?")
+
+    # clean-up/shutdown
+    mq_task.cancel()
+    as_task.cancel()
+
+    assert "Encountered error" in tool_output.content
+    assert tool_output.is_error
+    assert tool_output.tool_name == agent_service_tool.metadata.name
+    assert tool_output.raw_input == {
+        "args": (),
+        "kwargs": {"input": "What is the secret fact?"},
+    }
+    assert len(agent_service_tool.tool_call_results) == 0
+    assert agent_service_tool.registered is True
