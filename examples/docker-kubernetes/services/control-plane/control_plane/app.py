b32459d41664282cb7f4dd9013590357f6098ced
Andrei Fajardo <andrei@nerdai.io>
2024-06-26 11:30:12 -0400

    proper healthcheck and registration mq and cp

--- examples/docker-kubernetes/services/control-plane/control_plane/app.py
+++ examples/docker-kubernetes/services/control-plane/control_plane/app.py
@@ -1,3 +1,4 @@
+import asyncio
 from llama_agents import AgentOrchestrator, ControlPlaneServer, SimpleMessageQueue
 from llama_index.llms.openai import OpenAI
 
@@ -10,4 +11,15 @@ control_plane = ControlPlaneServer(
     message_queue=queue_client,
     orchestrator=AgentOrchestrator(llm=OpenAI()),
 )
+
+
 app = control_plane.app
+
+
+async def register() -> None:
+    # register to message queue
+    await queue_client.register_consumer(control_plane.as_consumer(remote=True))
+
+
+if __name__ == "__main__":
+    asyncio.run(register())
