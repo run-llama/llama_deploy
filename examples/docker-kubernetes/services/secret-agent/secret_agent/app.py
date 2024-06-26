b240df32354e9d3513cbe8340eba837bb3b7da04
Andrei Fajardo <andrei@nerdai.io>
2024-06-26 00:34:36 -0400

    docker image for secret-agent

---
+++ examples/docker-kubernetes/services/secret-agent/secret_agent/app.py
@@ -0,0 +1,31 @@
+from llama_agents import AgentService, SimpleMessageQueue
+
+from llama_index.core.agent import FunctionCallingAgentWorker
+from llama_index.core.tools import FunctionTool
+from llama_index.llms.openai import OpenAI
+
+
+# create an agent
+def get_the_secret_fact() -> str:
+    """Returns the secret fact."""
+    return "The secret fact is: A baby llama is called a 'Cria'."
+
+
+tool = FunctionTool.from_defaults(fn=get_the_secret_fact)
+worker = FunctionCallingAgentWorker.from_tools([tool], llm=OpenAI())
+agent = worker.as_agent()
+
+# create agent server
+message_queue = SimpleMessageQueue()
+queue_client = message_queue.client
+
+agent_server = AgentService(
+    agent=agent,
+    message_queue=queue_client,
+    description="Useful for getting the secret fact.",
+    service_name="secret_fact_agent",
+    host="127.0.0.1",
+    port=8000,
+)
+
+app = agent_server._app
