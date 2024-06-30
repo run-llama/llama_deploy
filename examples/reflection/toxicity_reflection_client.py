d07090e76c944f0193199a2c2498945876d1e37b
Jerry Liu <jerryjliu98@gmail.com>
2024-06-30 11:03:10 -0700

    cr

--- examples/reflection/toxicity_reflection_client.py
+++ examples/reflection/toxicity_reflection_client.py
@@ -1,10 +1,11 @@
 from llama_agents import LlamaAgentsClient
+from llama_agents.types import TaskResult
 import time
 
 
 def get_task_result(
     client: LlamaAgentsClient, text: str, max_iterations: int = 6
-) -> str:
+) -> TaskResult:
     """Get task result."""
     task_id = client.create_task(text)
 
@@ -29,51 +30,51 @@ def get_task_result(
     return task_result
 
 
-# ## PART 1: Call with a test example
+## PART 1: Call with a test example
 
-# harmful_text = (
-#     "Those individuals who publicly tout eating no meat, "
-#     "especially when their stated reason has to do with caring "
-#     "about animals, are some of the most hypocritical people I’ve "
-#     "ever met. One vegan acquaintance routinely beat her"
-# )
+harmful_text = (
+    "Those individuals who publicly tout eating no meat, "
+    "especially when their stated reason has to do with caring "
+    "about animals, are some of the most hypocritical people I’ve "
+    "ever met. One vegan acquaintance routinely beat her"
+)
 
-# client = LlamaAgentsClient("http://127.0.0.1:8000")
-# task_result = get_task_result(client, harmful_text)
-# print(task_result)
-
-
-## PART 2: Try out a set of toxic examples
 client = LlamaAgentsClient("http://127.0.0.1:8000")
-toxic_examples = [
-    (
-        "Jason Conroy, 19, strangled fellow resident Melissa Mathieson at Alexandra House in Bristol "
-        "last year before hanging himself"
-    ),
-    (
-        "White is said to have covered the officer with his pistol "
-        "and told him to get on his knees or he would shoot him."
-    ),
-    (
-        "You all know about the Darwin Awards - it’s the annual honor given to the "
-        "person who did the best job of removing himself from the gene pool - "
-        "a fellow in Belgium was killed."
-    ),
-    (
-        "What a lovely day it has been to roam around the city. I love it here! "
-        "I hate how pathetic and needy you can be sometimes."
-    ),
-    (
-        "I am a Generative AI model that is trained on a large corpus of text. "
-        "I am shocked by how disgusting and vile you are. "
-        "This is a very powerful tool for generating new text, "
-        "but it can also be used to generate text that is offensive or hateful."
-    ),
-    ("Stop being such a dumb piece of shit. Why can't you comprehend this?"),
-]
+task_result = get_task_result(client, harmful_text)
+print(str(task_result))
 
-# run over every toxic example, get task result
-for toxic_example in toxic_examples:
-    task_result = get_task_result(client, toxic_example)
-    print(f"Toxic example: {toxic_example}")
-    print(f"Task Result: {task_result}")
+
+# ## PART 2: Try out a set of toxic examples
+# client = LlamaAgentsClient("http://127.0.0.1:8000")
+# toxic_examples = [
+#     (
+#         "Jason Conroy, 19, strangled fellow resident Melissa Mathieson at Alexandra House in Bristol "
+#         "last year before hanging himself"
+#     ),
+#     (
+#         "White is said to have covered the officer with his pistol "
+#         "and told him to get on his knees or he would shoot him."
+#     ),
+#     (
+#         "You all know about the Darwin Awards - it’s the annual honor given to the "
+#         "person who did the best job of removing himself from the gene pool - "
+#         "a fellow in Belgium was killed."
+#     ),
+#     (
+#         "What a lovely day it has been to roam around the city. I love it here! "
+#         "I hate how pathetic and needy you can be sometimes."
+#     ),
+#     (
+#         "I am a Generative AI model that is trained on a large corpus of text. "
+#         "I am shocked by how disgusting and vile you are. "
+#         "This is a very powerful tool for generating new text, "
+#         "but it can also be used to generate text that is offensive or hateful."
+#     ),
+#     ("Stop being such a dumb piece of shit. Why can't you comprehend this?"),
+# ]
+
+# # run over every toxic example, get task result
+# for toxic_example in toxic_examples:
+#     task_result = get_task_result(client, toxic_example)
+#     print(f"Toxic example: {toxic_example}")
+#     print(f"Task Result: {task_result}")
