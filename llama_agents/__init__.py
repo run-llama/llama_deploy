f1f21e3fdc84cd64fc71e346dbac647ce1d18fc1
Jerry Liu <jerryjliu98@gmail.com>
2024-07-01 11:11:15 -0700

    Add query component service (#79)

--- llama_agents/__init__.py
+++ llama_agents/__init__.py
@@ -14,7 +14,12 @@ from llama_agents.tools import (
     ServiceComponent,
     ServiceTool,
 )
-from llama_agents.services import AgentService, ToolService, HumanService
+from llama_agents.services import (
+    AgentService,
+    ToolService,
+    HumanService,
+    ComponentService,
+)
 
 # configure logger
 import logging
@@ -38,6 +43,7 @@ __all__ = [
     "AgentService",
     "HumanService",
     "ToolService",
+    "ComponentService",
     # messages
     "QueueMessage",
     # message consumers
--- llama_agents/tools/service_component.py
+++ llama_agents/tools/service_component.py
@@ -1,25 +1,94 @@
 import json
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
+from llama_index.core.bridge.pydantic import PrivateAttr
 from llama_index.core.query_pipeline import CustomQueryComponent
+from llama_index.core.base.query_pipeline.query import InputKeys
 
 from llama_agents.types import ServiceDefinition
+from enum import Enum
+
+
+class ModuleType(str, Enum):
+    """Module types.
+
+    NOTE: this is to allow both agent services and component services to be stitched together with
+    the pipeline orchestrator.
+
+    Ideally there should not be more types.
+
+    """
+
+    AGENT = "agent"
+    COMPONENT = "component"
 
 
 class ServiceComponent(CustomQueryComponent):
     name: str
     description: str
 
+    # Store a set of input keys from upstream modules
+    # NOTE: no need to track the output keys, this is a fake module anyways
+    _cur_input_keys: InputKeys = PrivateAttr()
+
+    module_type: ModuleType = ModuleType.AGENT
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        input_keys: Optional[InputKeys] = None,
+        module_type: ModuleType = ModuleType.AGENT,
+    ) -> None:
+        super().__init__(name=name, description=description, module_type=module_type)
+        if input_keys is not None:
+            self._cur_input_keys = input_keys or InputKeys.from_keys({"input"})
+
     @classmethod
     def from_service_definition(
-        cls, service_def: ServiceDefinition
+        cls,
+        service_def: ServiceDefinition,
+        input_keys: Optional[InputKeys] = None,
+        module_type: ModuleType = ModuleType.AGENT,
     ) -> "ServiceComponent":
-        return cls(name=service_def.service_name, description=service_def.description)
+        return cls(
+            name=service_def.service_name,
+            description=service_def.description,
+            input_keys=input_keys,
+            module_type=module_type,
+        )
+
+    @classmethod
+    def from_component_service(
+        cls,
+        component_service: Any,
+    ) -> "ServiceComponent":
+        """Create a service component from a component service."""
+        from llama_agents.services.component import ComponentService
+
+        if not isinstance(component_service, ComponentService):
+            raise ValueError("component_service must be a Component")
+
+        component = component_service.component
+        return cls.from_service_definition(
+            component_service.service_definition,
+            input_keys=component.input_keys,
+            module_type=ModuleType.COMPONENT,
+        )
+
+    @property
+    def input_keys(self) -> InputKeys:
+        """Input keys."""
+        # NOTE: user can override this too, but we have them implement an
+        # abstract method to make sure they do it
+
+        return self._cur_input_keys
 
     @property
     def _input_keys(self) -> set:
         """Input keys dict."""
-        return {"input"}
+        # HACK: not used
+        return set()
 
     @property
     def _output_keys(self) -> set:
@@ -31,7 +100,7 @@ class ServiceComponent(CustomQueryComponent):
             {
                 "name": self.name,
                 "description": self.description,
-                **kwargs,
+                "input": kwargs,
             }
         )
         return {"service_output": json_dump}
@@ -42,7 +111,7 @@ class ServiceComponent(CustomQueryComponent):
             {
                 "name": self.name,
                 "description": self.description,
-                **kwargs,
+                "input": kwargs,
             }
         )
         return {"service_output": json_dump}
