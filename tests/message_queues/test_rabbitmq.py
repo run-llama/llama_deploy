104786492d9dae735c7af8948044777a4c13d505
Andrei Fajardo <andrei@nerdai.io>
2024-07-06 21:27:14 -0400

    add unit test for convenience constructor

--- tests/message_queues/test_rabbitmq.py
+++ tests/message_queues/test_rabbitmq.py
@@ -17,5 +17,27 @@ def test_init() -> None:
     assert mq.exchange_name == "test-exchange"
 
 
+def test_from_url_params() -> None:
+    # arrange
+    username = "mock-user"
+    password = "mock-pass"
+    host = "mock-host"
+    vhost = "mock-vhost"
+    exchange_name = "mock-exchange"
+
+    # act
+    mq = RabbitMQMessageQueue.from_url_params(
+        username=username,
+        password=password,
+        host=host,
+        vhost=vhost,
+        exchange_name=exchange_name,
+    )
+
+    # assert
+    assert mq.url == f"amqp://{username}:{password}@{host}/{vhost}"
+    assert mq.exchange_name == exchange_name
+
+
 # @pytest.mark.skipif(aio_pika is None, reason="aio_pika not installed")
 # @pytest.mark.asyncio()
