f9aba19adad2a5df2100932c0e402baa3e2afd7f
Andrei Fajardo <andrei@nerdai.io>
2024-07-20 10:20:05 -0400

    start KafkaMessageQueue and impl create topic

---
+++ llama_agents/message_queues/apache_kafka.py
@@ -0,0 +1,101 @@
+"""Apache Kafka Message Queue."""
+
+import asyncio
+from logging import getLogger
+from typing import Any, Callable, Coroutine, Dict, List, Optional
+from llama_agents.message_queues.base import (
+    BaseMessageQueue,
+)
+from llama_agents.messages.base import QueueMessage
+from llama_agents.message_consumers.base import (
+    BaseMessageQueueConsumer,
+    default_start_consuming_callable,
+)
+
+logger = getLogger(__name__)
+
+
+DEFAULT_URL = "localhost:9092"
+DEFAULT_TOPIC_PARTITIONS = 10
+DEFAULT_TOPIC_REPLICATION_FACTOR = 1
+
+
+class KafkaMessageQueue(BaseMessageQueue):
+    """Apache Kafka integration with aiokafka."""
+
+    url: str = DEFAULT_URL
+
+    def __init__(
+        self,
+        url: str = DEFAULT_URL,
+    ) -> None:
+        super().__init__(url=url)
+
+    @classmethod
+    def from_url_params(
+        cls,
+        host: str,
+        port: Optional[int] = None,
+    ) -> "KafkaMessageQueue":
+        url = f"{host}:{port}" if port else f"{host}"
+        return cls(url=url)
+
+    def _create_new_topic(
+        self,
+        topic_name: str,
+        num_partitions: Optional[int] = None,
+        replication_factor: Optional[int] = None,
+        **kwargs: Dict[str, Any],
+    ) -> None:
+        """Create a new topic."""
+        from kafka.admin import KafkaAdminClient, NewTopic
+        from kafka.errors import TopicAlreadyExistsError
+
+        admin_client = KafkaAdminClient(bootstrap_servers=self.url)
+        try:
+            topic = NewTopic(
+                name=topic_name,
+                num_partitions=num_partitions or DEFAULT_TOPIC_PARTITIONS,
+                replication_factor=replication_factor
+                or DEFAULT_TOPIC_REPLICATION_FACTOR,
+                **kwargs,
+            )
+            admin_client.create_topics(new_topics=[topic])
+            logger.info(f"New topic {topic_name} created.")
+        except TopicAlreadyExistsError:
+            logger.info(f"Topic {topic_name} already exists.")
+            pass
+
+    async def _publish(self, message: QueueMessage) -> Any:
+        ...
+
+    async def cleanup_local(
+        self, message_types: List[str], *args: Any, **kwargs: Dict[str, Any]
+    ) -> None:
+        ...
+
+    async def deregister_consumer(self, consumer: BaseMessageQueueConsumer) -> Any:
+        ...
+
+    async def launch_local(self) -> asyncio.Task:
+        return asyncio.create_task(self.processing_loop())
+
+    async def launch_server(self) -> None:
+        ...
+
+    async def processing_loop(self) -> None:
+        pass
+
+    async def register_consumer(
+        self, consumer: BaseMessageQueueConsumer
+    ) -> Callable[..., Coroutine[Any, Any, None]]:
+        return default_start_consuming_callable
+
+
+async def main() -> None:
+    mq = KafkaMessageQueue()
+    mq._create_new_topic(topic_name="test")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
