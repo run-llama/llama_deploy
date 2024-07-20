c1e857ad0134d1e92e3ac9f8101d083c7c69d5ba
Andrei Fajardo <andrei@nerdai.io>
2024-07-20 15:09:04 -0400

    clean up

--- examples/kafka/simple-scripts/local_launcher_single.py
+++ examples/kafka/simple-scripts/local_launcher_single.py
@@ -1,23 +1,18 @@
 from llama_agents import (
     AgentService,
+    HumanService,
     ControlPlaneServer,
     PipelineOrchestrator,
     ServiceComponent,
+    LocalLauncher,
 )
-from llama_agents.tools import ServiceAsTool
 from llama_agents.message_queues.apache_kafka import KafkaMessageQueue
-from llama_agents.launchers.local import LocalLauncher
-
 
 from llama_index.core.agent import FunctionCallingAgentWorker
 from llama_index.core.tools import FunctionTool
-from llama_index.core.query_pipeline import QueryPipeline
+from llama_index.core.query_pipeline import RouterComponent, QueryPipeline
 from llama_index.llms.openai import OpenAI
-from llama_index.agent.openai import OpenAIAgent
-
-
-# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+from llama_index.core.selectors import PydanticSingleSelector
 
 
 # create an agent
@@ -28,44 +23,40 @@ def get_the_secret_fact() -> str:
 
 tool = FunctionTool.from_defaults(fn=get_the_secret_fact)
 
-worker1 = FunctionCallingAgentWorker.from_tools([tool], llm=OpenAI())
-agent1 = worker1.as_agent()
-
 # create our multi-agent framework components
 message_queue = KafkaMessageQueue()
 
-agent1_server = AgentService(
-    agent=agent1,
+worker = FunctionCallingAgentWorker.from_tools([tool], llm=OpenAI())
+agent = worker.as_agent()
+agent_service = AgentService(
+    agent=agent,
     message_queue=message_queue,
     description="Useful for getting the secret fact.",
     service_name="secret_fact_agent",
 )
+agent_component = ServiceComponent.from_service_definition(agent_service)
 
-agent1_server_tool = ServiceAsTool.from_service_definition(
-    message_queue=message_queue, service_definition=agent1_server.service_definition
+human_service = HumanService(
+    message_queue=message_queue, description="Answers queries about math."
 )
+human_component = ServiceComponent.from_service_definition(human_service)
 
-agent2 = OpenAIAgent.from_tools(
-    [agent1_server_tool],
-    system_prompt="Perform the task, return the result as well as a funny joke.",
-)  # worker2.as_agent()
-agent2_server = AgentService(
-    agent=agent2,
-    message_queue=message_queue,
-    description="Useful for telling funny jokes.",
-    service_name="dumb_fact_agent",
+pipeline = QueryPipeline(
+    chain=[
+        RouterComponent(
+            selector=PydanticSingleSelector.from_defaults(llm=OpenAI()),
+            choices=[agent_service.description, human_service.description],
+            components=[agent_component, human_component],
+        )
+    ]
 )
-
-agent2_component = ServiceComponent.from_service_definition(agent2_server)
-
-pipeline = QueryPipeline(chain=[agent2_component])
 
 pipeline_orchestrator = PipelineOrchestrator(pipeline)
 
 control_plane = ControlPlaneServer(message_queue, pipeline_orchestrator)
 
 # launch it
-launcher = LocalLauncher([agent1_server, agent2_server], control_plane, message_queue)
-result = launcher.launch_single("What is the secret fact?")
+launcher = LocalLauncher([agent_service, human_service], control_plane, message_queue)
+result = launcher.launch_single("What is 1 + 2 + 3 + 4 + 5?")
 
 print(f"Result: {result}")
