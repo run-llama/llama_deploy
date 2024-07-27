d2d2ec1c8bcfa5920daa066369c7853f7b412a14
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-07-27 16:40:37 -0400

    Revert mistakenly merged AWS SQS PR (#160)
    * Revert "remove duplicate"
    
    This reverts commit b6d9452af4c9dbc7d82376de32a355aadb60c0f7.
    
    * Revert "initial push, working with a restart"
    
    This reverts commit be52f7b7b76eb166c79bd48d896f9293f4974b40.

--- llama_agents/message_queues/aws_sqs/__init__.py
+++
@@ -1,4 +0,0 @@
-from llama_agents.message_queues.aws_sqs.aws_sqs import SQSMessageQueue
-
-
-__all__ = ["SQSMessageQueue"]
--- llama_agents/message_queues/aws_sqs/base.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class BaseAWSResource(BaseModel):
-    arn: str
