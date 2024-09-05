2e36ef7d03fc74bcc51ad1cbd9899b5b2acce05c
Andrei Fajardo <andrei@nerdai.io>
2024-09-05 00:21:24 -0400

    allow for disable of deployment in deploy_core

--- examples/message-queue-integrations/multi-workflows-app/multi_workflows_app/deployment/core.py
+++ examples/message-queue-integrations/multi-workflows-app/multi_workflows_app/deployment/core.py
@@ -38,6 +38,16 @@ if __name__ == "__main__":
         type=str,
         help="The message queue to use.",
     )
+    parser.add_argument(
+        "--disable-message-queue",
+        action="store_true",
+        help="Whether to use the cached results or not.",
+    )
+    parser.add_argument(
+        "--disable-control-plane",
+        action="store_true",
+        help="Whether to use the cached results or not.",
+    )
     args = parser.parse_args()
 
     message_queue_config = message_queue_configs[args.message_queue]
@@ -45,5 +55,7 @@ if __name__ == "__main__":
         deploy_core(
             control_plane_config=control_plane_config,
             message_queue_config=message_queue_config,
+            disable_message_queue=args.disable_message_queue,
+            disable_control_plane=args.disable_control_plane,
         )
     )
