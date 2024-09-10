26684e6fab302990f36383009e9787bb33b512a0
Massimiliano Pippi <mpippi@gmail.com>
2024-09-10 15:27:35 +0200

    make entrypoint smarter

--- docker/message_queue.py
+++ docker/message_queue.py
@@ -1,14 +1,24 @@
 import asyncio
-from llama_deploy import (
-    deploy_core,
-)
+import os
+
+from llama_deploy import deploy_core
+from llama_deploy.message_queues.simple import SimpleMessageQueueConfig
+from llama_deploy.message_queues.apache_kafka import KafkaMessageQueueConfig
+from llama_deploy.message_queues.rabbitmq import RabbitMQMessageQueueConfig
+from llama_deploy.message_queues.redis import RedisMessageQueueConfig
+
+
+CONFIGS = {
+    "kafka": KafkaMessageQueueConfig(),
+    "rabbitmq": RabbitMQMessageQueueConfig(),
+    "redis": RedisMessageQueueConfig(),
+    "simple": SimpleMessageQueueConfig(),
+}
 
 
 async def run_deploy() -> None:
     await deploy_core(
-        # All configs are optional.
-        # In this case, the env vars in the docker-compose file are used.
-        # message_queue_config=MessageQueueConfig(),
+        message_queue_config=os.environ.get("MESSAGE_QUEUE_CONFIG", "simple"),
         disable_control_plane=True,
     )
 
