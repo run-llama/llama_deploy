55c68471b9a239b928413a63a83c16ac8d9d0784
Massimiliano Pippi <mpippi@gmail.com>
2024-09-26 18:14:22 +0200

    fix module name and add docstrings

--- llama_deploy/apiserver/routers/deployments.py
+++ llama_deploy/apiserver/routers/deployments.py
@@ -5,13 +5,14 @@ from llama_deploy.apiserver.server import manager
 from llama_deploy.apiserver.config_parser import Config
 
 
-deploy_router = APIRouter(
+deployments_router = APIRouter(
     prefix="/deployments",
 )
 
 
-@deploy_router.get("/")
+@deployments_router.get("/")
 async def read_deployments() -> JSONResponse:
+    """Returns a list of active deployments."""
     return JSONResponse(
         {
             "deployments": list(manager._deployments.keys()),
@@ -19,9 +20,9 @@ async def read_deployments() -> JSONResponse:
     )
 
 
-@deploy_router.get("/{deployment_name}")
+@deployments_router.get("/{deployment_name}")
 async def read_deployment(deployment_name: str) -> JSONResponse:
-    print(manager.deployment_names)
+    """Returns the details of a specific deployment."""
     if deployment_name not in manager.deployment_names:
         raise HTTPException(status_code=404, detail="Deployment not found")
 
@@ -32,8 +33,9 @@ async def read_deployment(deployment_name: str) -> JSONResponse:
     )
 
 
-@deploy_router.post("/create/")
+@deployments_router.post("/create/")
 async def create_deployment(config_file: UploadFile = File(...)) -> JSONResponse:
+    """Creates a new deployment by uploading a configuration file."""
     config = Config.from_yaml_bytes(await config_file.read())
     manager.deploy(config)
 
