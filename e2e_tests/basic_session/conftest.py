1878cd05375997657c5e2e224cd3e4dcc9f8e1f9
Logan Markewich <logan.markewich@live.com>
2024-10-14 16:09:08 -0600

    fix e2e tests

---
+++ e2e_tests/basic_session/conftest.py
@@ -0,0 +1,55 @@
+import asyncio
+import multiprocessing
+import time
+
+import pytest
+
+from llama_deploy import (
+    ControlPlaneConfig,
+    SimpleMessageQueueConfig,
+    WorkflowServiceConfig,
+    deploy_core,
+    deploy_workflow,
+)
+
+from .workflow import SessionWorkflow
+
+
+def run_async_core():
+    asyncio.run(deploy_core(ControlPlaneConfig(), SimpleMessageQueueConfig()))
+
+
+@pytest.fixture(scope="package")
+def core():
+    p = multiprocessing.Process(target=run_async_core)
+    p.start()
+    time.sleep(5)
+
+    yield
+
+    p.kill()
+
+
+def run_async_workflow():
+    asyncio.run(
+        deploy_workflow(
+            SessionWorkflow(timeout=10),
+            WorkflowServiceConfig(
+                host="127.0.0.1",
+                port=8002,
+                service_name="session_workflow",
+            ),
+            ControlPlaneConfig(),
+        )
+    )
+
+
+@pytest.fixture(scope="package")
+def workflow(core):
+    p = multiprocessing.Process(target=run_async_workflow)
+    p.start()
+    time.sleep(5)
+
+    yield
+
+    p.kill()
