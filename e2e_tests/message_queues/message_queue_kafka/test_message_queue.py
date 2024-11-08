a7e9b118afb172bfad01637a95510d3ec3b0b196
Massimiliano Pippi <mpippi@gmail.com>
2024-11-08 09:08:41 +0100

    do not use docker-compose alias

--- e2e_tests/message_queues/message_queue_kafka/test_message_queue.py
+++ e2e_tests/message_queues/message_queue_kafka/test_message_queue.py
@@ -13,11 +13,11 @@ from llama_deploy.messages import QueueMessage
 def kafka_service():
     compose_file = Path(__file__).resolve().parent / "docker-compose.yml"
     proc = subprocess.Popen(
-        ["docker-compose", "-f", f"{compose_file}", "up", "-d", "--wait"]
+        ["docker", "compose", "-f", f"{compose_file}", "up", "-d", "--wait"]
     )
     proc.communicate()
     yield
-    subprocess.Popen(["docker-compose", "-f", f"{compose_file}", "down"])
+    subprocess.Popen(["docker", "compose", "-f", f"{compose_file}", "down"])
 
 
 @pytest.fixture
