45df7da09ecf7d99b052f27a3fc04b11b7409d0c
Massimiliano Pippi <mpippi@gmail.com>
2024-11-13 18:41:33 +0100

    change testing technique

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-from typing import Any
-from unittest import mock
 
 import pytest
 from click.testing import CliRunner
@@ -14,9 +12,3 @@ def data_path() -> Path:
 @pytest.fixture
 def runner() -> CliRunner:
     return CliRunner()
-
-
-@pytest.fixture
-def httpx_request() -> Any:
-    with mock.patch("llama_deploy.client.base.httpx") as mocked_httpx:
-        yield mocked_httpx.AsyncClient.return_value.__aenter__.return_value.request
--- tests/cli/test_deploy.py
+++ tests/cli/test_deploy.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Any
 from unittest import mock
 
 import httpx
@@ -8,35 +7,36 @@ from click.testing import CliRunner
 from llama_deploy.cli import llamactl
 
 
-def test_deploy(runner: CliRunner, data_path: Path, httpx_request: Any) -> None:
+def test_deploy(runner: CliRunner, data_path: Path) -> None:
     test_config_file = data_path / "deployment.yaml"
-    mocked_response = mock.MagicMock(
-        status_code=200, json=lambda: {"name": "test_deployment"}
-    )
-
-    httpx_request.return_value = mocked_response
-    result = runner.invoke(llamactl, ["-t", "5.0", "deploy", str(test_config_file)])
-
-    print(result.output)
-    assert result.exit_code == 0
-    with open(test_config_file, "rb") as f:
-        httpx_request.assert_called_with(
-            "POST",
-            "http://localhost:4501/deployments/create",
-            files={"config_file": f.read()},
-            timeout=5.0,
+    mocked_result = mock.MagicMock(id="test_deployment")
+    with mock.patch("llama_deploy.cli.deploy.Client") as mocked_client:
+        mocked_client.return_value.sync.apiserver.deployments.create.return_value = (
+            mocked_result
         )
 
+        result = runner.invoke(llamactl, ["-t", "5.0", "deploy", str(test_config_file)])
 
-def test_deploy_failed(runner: CliRunner, data_path: Path, httpx_request: Any) -> None:
+        assert result.exit_code == 0
+        assert result.output == "Deployment successful: test_deployment\n"
+        mocked_client.assert_called_with(
+            api_server_url="http://localhost:4501", disable_ssl=False, timeout=5.0
+        )
+        file_arg = (
+            mocked_client.return_value.sync.apiserver.deployments.create.call_args
+        )
+        assert str(test_config_file) == file_arg.args[0].name
+
+
+def test_deploy_failed(runner: CliRunner, data_path: Path) -> None:
     test_config_file = data_path / "deployment.yaml"
-    mocked_response = mock.MagicMock(
-        status_code=401, json=lambda: {"detail": "Unauthorized!"}
-    )
-    httpx_request.side_effect = httpx.HTTPStatusError(
-        "Unauthorized!", response=mocked_response, request=httpx.Request("", "")
-    )
+    with mock.patch("llama_deploy.cli.deploy.Client") as mocked_client:
+        mocked_client.return_value.sync.apiserver.deployments.create.side_effect = (
+            httpx.HTTPStatusError(
+                "Unauthorized!", response=mock.MagicMock(), request=mock.MagicMock()
+            )
+        )
 
-    result = runner.invoke(llamactl, ["deploy", str(test_config_file)])
-    assert result.exit_code == 1
-    assert result.output == "Error: Unauthorized!\n"
+        result = runner.invoke(llamactl, ["deploy", str(test_config_file)])
+        assert result.exit_code == 1
+        assert result.output == "Error: Unauthorized!\n"
