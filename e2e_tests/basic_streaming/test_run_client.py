f61e5836366a6a5e7faba4e1ddfb724e64e9598f
Andrei Fajardo <andrei@nerdai.io>
2024-12-12 11:39:10 -0500

    revert back previous changes

--- e2e_tests/basic_streaming/test_run_client.py
+++ e2e_tests/basic_streaming/test_run_client.py
@@ -5,7 +5,7 @@ from llama_deploy import Client
 
 @pytest.mark.e2e
 def test_run_client(services):
-    client = Client(timeout=20)
+    client = Client(timeout=10)
 
     # sanity check
     sessions = client.sync.core.sessions.list()
@@ -39,7 +39,7 @@ def test_run_client(services):
 @pytest.mark.e2e
 @pytest.mark.asyncio
 async def test_run_client_async(services):
-    client = Client(timeout=20)
+    client = Client(timeout=10)
 
     # test streaming
     session = await client.core.sessions.create()
