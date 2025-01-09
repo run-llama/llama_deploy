195ca88fdef9dff88ac67599fa1b5228efcd80bb
Massimiliano Pippi <mpippi@gmail.com>
2025-01-09 10:03:36 +0100

    refact: make topic configurable for SolaceMessageQueue (#424)

--- tests/message_queues/test_solace.py
+++ tests/message_queues/test_solace.py
@@ -110,10 +110,10 @@ def solace_queue(
         mock_messaging_service.create_persistent_message_publisher_builder.return_value.build.return_value = mock_publisher
         mock_messaging_service.create_persistent_message_receiver_builder.return_value.with_missing_resources_creation_strategy.return_value.build.return_value = mock_receiver
 
-        queue = SolaceMessageQueue(type="solace")
-        queue.messaging_service = mock_messaging_service
-        queue.publisher = mock_publisher
-        queue.persistent_receiver = mock_receiver
+        queue = SolaceMessageQueue(SolaceMessageQueueConfig(type="solace"))
+        queue._messaging_service = mock_messaging_service
+        queue._publisher = mock_publisher
+        queue._persistent_receiver = mock_receiver
         yield queue
 
 
@@ -140,7 +140,7 @@ async def test_establish_connection(
     assert connect_result is not None
     mock_messaging_service.connect.assert_called_once()
     mock_publisher.start.assert_called_once()
-    assert solace_queue.publisher.set_message_publish_receipt_listener.called
+    assert solace_queue._publisher.set_message_publish_receipt_listener.called  # type: ignore
 
 
 @pytest.mark.asyncio
