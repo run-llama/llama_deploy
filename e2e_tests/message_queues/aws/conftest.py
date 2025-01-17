6ee7f4ab725e19889194d4bc8fedb1f4ed3f1175
Massimiliano Pippi <mpippi@gmail.com>
2025-01-17 16:30:16 +0100

    ci: use different queues to allow tests to run in parallel

--- e2e_tests/message_queues/aws/conftest.py
+++ e2e_tests/message_queues/aws/conftest.py
@@ -1,5 +1,6 @@
 import asyncio
 import multiprocessing
+import os
 import time
 
 import pytest
@@ -20,7 +21,13 @@ def mq():
     return AWSMessageQueue()
 
 
-def run_workflow_one():
+@pytest.fixture
+def topic_prefix() -> str:
+    """Use different SQS queues to allow tests to run in parallel"""
+    return os.environ.get("TOPIC_PREFIX", "")
+
+
+def run_workflow_one(topic_prefix):
     asyncio.run(
         deploy_workflow(
             BasicWorkflow(timeout=10, name="Workflow one"),
@@ -29,12 +36,12 @@ def run_workflow_one():
                 port=8003,
                 service_name="basic",
             ),
-            ControlPlaneConfig(topic_namespace="core_one", port=8001),
+            ControlPlaneConfig(topic_namespace=f"{topic_prefix}core_one", port=8001),
         )
     )
 
 
-def run_workflow_two():
+def run_workflow_two(topic_prefix):
     asyncio.run(
         deploy_workflow(
             BasicWorkflow(timeout=10, name="Workflow two"),
@@ -43,24 +50,24 @@ def run_workflow_two():
                 port=8004,
                 service_name="basic",
             ),
-            ControlPlaneConfig(topic_namespace="core_two", port=8002),
+            ControlPlaneConfig(topic_namespace=f"{topic_prefix}core_two", port=8002),
         )
     )
 
 
-def run_core_one():
+def run_core_one(topic_prefix):
     asyncio.run(
         deploy_core(
-            ControlPlaneConfig(topic_namespace="core_one", port=8001),
+            ControlPlaneConfig(topic_namespace=f"{topic_prefix}core_one", port=8001),
             AWSMessageQueueConfig(),
         )
     )
 
 
-def run_core_two():
+def run_core_two(topic_prefix):
     asyncio.run(
         deploy_core(
-            ControlPlaneConfig(topic_namespace="core_two", port=8002),
+            ControlPlaneConfig(topic_namespace=f"{topic_prefix}core_two", port=8002),
             AWSMessageQueueConfig(),
         )
     )
