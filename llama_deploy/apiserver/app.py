d40421f61070f7ca1e4f5197566faeee47e6937b
Massimiliano Pippi <mpippi@gmail.com>
2025-01-21 10:44:48 +0100

    fix: remove problematic root_path (#434)

--- llama_deploy/apiserver/app.py
+++ llama_deploy/apiserver/app.py
@@ -2,16 +2,16 @@ import logging
 import os
 
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
-from .routers import status_router, deployments_router
+from .routers import deployments_router, status_router
 from .server import lifespan
 
 logger = logging.getLogger("uvicorn.info")
 
 
-app = FastAPI(root_path="/", lifespan=lifespan)
+app = FastAPI(lifespan=lifespan)
 
 # Configure CORS middleware if the environment variable is set
 if not os.environ.get("DISABLE_CORS", False):
