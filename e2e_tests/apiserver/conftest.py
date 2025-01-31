361ae5f6f5a8a331e6b29a75dcf815195750f3a8
Massimiliano Pippi <mpippi@gmail.com>
2025-01-31 12:00:24 +0100

    add exp backoff

--- e2e_tests/apiserver/conftest.py
+++ e2e_tests/apiserver/conftest.py
@@ -2,6 +2,7 @@ import multiprocessing
 import time
 from pathlib import Path
 
+import httpx
 import pytest
 import uvicorn
 
@@ -12,11 +13,23 @@ def run_apiserver():
     uvicorn.run("llama_deploy.apiserver:app", host="127.0.0.1", port=4501)
 
 
+def wait_for_healthcheck():
+    while True:
+        retries = 1
+        try:
+            httpx.get("http://127.0.0.1:4501/status/")
+            break
+        except httpx.ConnectError:
+            if retries > 5:
+                raise
+            time.sleep(retries)
+
+
 @pytest.fixture(scope="function")
 def apiserver():
     p = multiprocessing.Process(target=run_apiserver)
     p.start()
-    time.sleep(5)
+    wait_for_healthcheck()
 
     yield
 
@@ -33,7 +46,7 @@ def apiserver_with_rc(monkeypatch):
 
     p = multiprocessing.Process(target=run_apiserver)
     p.start()
-    time.sleep(5)
+    wait_for_healthcheck()
 
     yield
 
