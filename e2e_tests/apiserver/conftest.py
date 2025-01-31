2c3b388860992c3d6f6fc3cddc0c911ded91586f
Massimiliano Pippi <mpippi@gmail.com>
2025-01-31 11:05:06 +0100

    fix e2e tests

--- e2e_tests/apiserver/conftest.py
+++ e2e_tests/apiserver/conftest.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import time
+from pathlib import Path
 
 import pytest
 import uvicorn
@@ -7,19 +8,36 @@ import uvicorn
 from llama_deploy.client import Client
 
 
-def run_async_apiserver():
+def run_apiserver():
     uvicorn.run("llama_deploy.apiserver:app", host="127.0.0.1", port=4501)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def apiserver():
-    p = multiprocessing.Process(target=run_async_apiserver)
+    p = multiprocessing.Process(target=run_apiserver)
     p.start()
     time.sleep(3)
 
     yield
 
-    p.terminate()
+    p.kill()
+    p.join()
+    p.close()
+
+
+@pytest.fixture(scope="function")
+def apiserver_with_rc(monkeypatch):
+    here = Path(__file__).parent
+    rc_path = here / "rc"
+    monkeypatch.setenv("LLAMA_DEPLOY_APISERVER_RC_PATH", str(rc_path))
+
+    p = multiprocessing.Process(target=run_apiserver)
+    p.start()
+    time.sleep(5)
+
+    yield
+
+    p.kill()
     p.join()
     p.close()
 
--- e2e_tests/apiserver/test_autodeploy.py
+++ e2e_tests/apiserver/test_autodeploy.py
@@ -1,35 +1,12 @@
-import asyncio
-from pathlib import Path
-
 import pytest
-import uvicorn
 
-from llama_deploy.apiserver.app import app
 from llama_deploy.apiserver.settings import ApiserverSettings
 
 
 @pytest.mark.asyncio
-async def test_autodeploy(client, monkeypatch):
-    here = Path(__file__).parent
-    rc_path = here / "rc"
-    monkeypatch.setenv("LLAMA_DEPLOY_APISERVER_RC_PATH", str(rc_path))
+async def test_autodeploy(client, apiserver_with_rc):
     settings = ApiserverSettings()
-    assert settings.rc_path == rc_path
-
-    cfg = uvicorn.Config(app, host=settings.host, port=settings.port)
-    server = uvicorn.Server(cfg)
-    server_task = asyncio.create_task(server.serve())
-
-    await asyncio.sleep(3)
+    assert str(settings.rc_path).endswith("llama_deploy/e2e_tests/apiserver/rc")
 
     status = await client.apiserver.status()
-
-    # tear down
-    try:
-        await server.shutdown()
-        server_task.cancel()
-    except asyncio.CancelledError:
-        pass
-
-    # assert
     assert "AutoDeployed" in status.deployments
