424ff66da01d44fe5496d0ed3310166a325e7210
Massimiliano Pippi <mpippi@gmail.com>
2025-02-18 12:18:24 +0100

    feat: proxy Prometheus metrics endpoint through API Server (#469)
    * feat: proxy Prometheus metrics endpoint through API Server
    
    * Update status.py

--- tests/apiserver/routers/test_status.py
+++ tests/apiserver/routers/test_status.py
@@ -1,3 +1,7 @@
+from typing import Any
+from unittest import mock
+
+import httpx
 from fastapi.testclient import TestClient
 
 
@@ -10,3 +14,30 @@ def test_read_main(http_client: TestClient) -> None:
         "status": "Healthy",
         "status_message": "",
     }
+
+
+def test_prom_proxy_off(http_client: TestClient, monkeypatch: Any) -> None:
+    monkeypatch.setenv("LLAMA_DEPLOY_APISERVER_PROMETHEUS_ENABLED", "false")
+    response = http_client.get("/status/metrics/")
+    assert response.status_code == 204
+    assert response.text == ""
+
+
+def test_prom_proxy(http_client: TestClient) -> None:
+    mock_metrics_response = 'metric1{label="value"} 1.0\nmetric2{label="value"} 2.0'
+    mock_response = httpx.Response(200, text=mock_metrics_response)
+
+    with mock.patch("httpx.AsyncClient.get", return_value=mock_response):
+        response = http_client.get("/status/metrics")
+        assert response.status_code == 200
+        assert response.text == mock_metrics_response
+
+
+def test_prom_proxy_failure(http_client: TestClient) -> None:
+    # Mock the HTTP client to raise an exception
+    with mock.patch(
+        "httpx.AsyncClient.get", side_effect=httpx.RequestError("Connection failed")
+    ):
+        response = http_client.get("/status/metrics")
+        assert response.status_code == 500
+        assert response.json()["detail"] == "Connection failed"
