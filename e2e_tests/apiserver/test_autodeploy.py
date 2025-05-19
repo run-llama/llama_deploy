0ba20d65b5686d6a0e8d9b91ec7e2d7d40cd17df
Massimiliano Pippi <mpippi@gmail.com>
2025-05-19 13:05:40 +0200

    fix e2e tests

--- e2e_tests/apiserver/test_autodeploy.py
+++ e2e_tests/apiserver/test_autodeploy.py
@@ -1,11 +1,7 @@
 import pytest
 
-from llama_deploy.apiserver.settings import settings
-
 
 @pytest.mark.asyncio
 async def test_autodeploy(client, apiserver_with_rc):
-    assert str(settings.rc_path).endswith("llama_deploy/e2e_tests/apiserver/rc")
-
     status = await client.apiserver.status()
     assert "AutoDeployed" in status.deployments
