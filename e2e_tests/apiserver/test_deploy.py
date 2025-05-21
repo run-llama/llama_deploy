0eaa7f93721dcb66d16e2c0cb06e9ff42ffde5e1
Massimiliano Pippi <mpippi@gmail.com>
2025-05-21 14:07:36 +0200

    add a test for local deployments

--- e2e_tests/apiserver/test_deploy.py
+++ e2e_tests/apiserver/test_deploy.py
@@ -15,7 +15,17 @@ async def test_deploy(apiserver, client):
 
 def test_deploy_sync(apiserver, client):
     here = Path(__file__).parent
-    with open(here / "deployments" / "deployment2.yml") as f:
+    with open(here / "deployments" / "deployment1.yml") as f:
         client.sync.apiserver.deployments.create(f)
 
-    assert "TestDeployment2" in client.sync.apiserver.status().deployments
+    assert "TestDeployment1" in client.sync.apiserver.status().deployments
+
+
+@pytest.mark.asyncio
+async def test_deploy_local(apiserver, client):
+    here = Path(__file__).parent
+    with open(here / "deployments" / "deployment2.yml") as f:
+        await client.apiserver.deployments.create(f)
+
+    status = await client.apiserver.status()
+    assert "TestDeployment2" in status.deployments
