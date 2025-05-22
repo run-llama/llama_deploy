2f1f29c9c4475add8014947dfd9d8317ca0e4cc6
Massimiliano Pippi <mpippi@gmail.com>
2025-05-22 16:11:31 +0200

    fix: ship node with Docker image

--- llama_deploy/cli/deploy.py
+++ llama_deploy/cli/deploy.py
@@ -10,12 +10,20 @@ from .internal.config import ConfigProfile
 @click.command()
 @click.pass_obj  # config_profile
 @click.option("--reload", is_flag=True)
+@click.option(
+    "--base-path",
+    required=False,
+    type=click.Path(file_okay=False, resolve_path=True, path_type=Path),  # type: ignore
+)
 @click.argument(
     "deployment_config_file",
     type=click.Path(dir_okay=False, resolve_path=True, path_type=Path),  # type: ignore
 )
 def deploy(
-    config_profile: ConfigProfile, reload: bool, deployment_config_file: Path
+    config_profile: ConfigProfile,
+    reload: bool,
+    deployment_config_file: Path,
+    base_path: Path | None,
 ) -> None:
     """Create or reload a deployment."""
     client = Client(
@@ -24,11 +32,13 @@ def deploy(
         timeout=config_profile.timeout,
     )
 
+    final_base_path = base_path or deployment_config_file.parent
+
     try:
         with open(deployment_config_file, "rb") as f:
             deployment = client.sync.apiserver.deployments.create(
                 f,
-                base_path=deployment_config_file.parent,
+                base_path=final_base_path,
                 reload=reload,
             )
     except Exception as e:
