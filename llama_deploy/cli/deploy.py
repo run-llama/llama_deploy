069d0ef626f838161014307556d1fab0219a1ba7
Massimiliano Pippi <mpippi@gmail.com>
2025-05-22 08:43:50 +0200

    fix: pass base_path from the deploy command in llamactl

--- llama_deploy/cli/deploy.py
+++ llama_deploy/cli/deploy.py
@@ -1,4 +1,4 @@
-from typing import IO
+from pathlib import Path
 
 import click
 
@@ -10,9 +10,12 @@ from .internal.config import ConfigProfile
 @click.command()
 @click.pass_obj  # config_profile
 @click.option("--reload", is_flag=True)
-@click.argument("deployment_config_file", type=click.File("rb"))
+@click.argument(
+    "deployment_config_file",
+    type=click.Path(dir_okay=False, resolve_path=True, path_type=Path),  # type: ignore
+)
 def deploy(
-    config_profile: ConfigProfile, reload: bool, deployment_config_file: IO
+    config_profile: ConfigProfile, reload: bool, deployment_config_file: Path
 ) -> None:
     """Create or reload a deployment."""
     client = Client(
@@ -22,9 +25,12 @@ def deploy(
     )
 
     try:
-        deployment = client.sync.apiserver.deployments.create(
-            deployment_config_file, reload
-        )
+        with open(deployment_config_file, "rb") as f:
+            deployment = client.sync.apiserver.deployments.create(
+                f,
+                base_path=deployment_config_file.parent,
+                reload=reload,
+            )
     except Exception as e:
         raise click.ClickException(str(e))
 
