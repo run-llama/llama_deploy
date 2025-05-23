98515486351931a3932047cbfb6469b3c3c46fc8
Massimiliano Pippi <mpippi@gmail.com>
2025-05-23 09:46:15 +0200

    chore: remove deprecated clients (#508)

--- llama_deploy/client/__init__.py
+++ llama_deploy/client/__init__.py
@@ -1,5 +1,3 @@
-from .async_client import AsyncLlamaDeployClient
-from .sync_client import LlamaDeployClient
 from .client import Client
 
-__all__ = ["AsyncLlamaDeployClient", "Client", "LlamaDeployClient"]
+__all__ = ["Client"]
