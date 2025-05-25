998c90286f0cce58b1351ee41638d18e61ccfbc6
Massimiliano Pippi <mpippi@gmail.com>
2025-05-25 19:46:02 +0200

    fix roundtrip tests

--- e2e_tests/message_queues/rabbitmq/test_message_queue.py
+++ e2e_tests/message_queues/rabbitmq/test_message_queue.py
@@ -5,34 +5,18 @@ import pytest
 from llama_deploy import Client
 from llama_deploy.messages import QueueMessage
 
-from ..conftest import CallableMessageConsumer
-
 
 @pytest.mark.asyncio
 async def test_roundtrip(mq):
-    received_messages = []
-
-    # register a consumer
-    def message_handler(message: QueueMessage) -> None:
-        received_messages.append(message)
-
-    test_consumer = CallableMessageConsumer(
-        message_type="test_message", handler=message_handler
-    )
-    start_consuming_callable = await mq.register_consumer(test_consumer, topic="test")
-
     # produce a message
     test_message = QueueMessage(type="test_message", data={"message": "this is a test"})
     await mq.publish(test_message, topic="test")
 
-    # consume the message
-    t = asyncio.create_task(start_consuming_callable())
     await asyncio.sleep(1)
-    t.cancel()
-    await t
 
-    assert len(received_messages) == 1
-    assert test_message in received_messages
+    async for m in mq.get_message("test"):
+        assert m == test_message
+        break
 
 
 @pytest.mark.asyncio
