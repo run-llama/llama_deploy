cbefd67d99c07ab83bac9c4e1e713b2ce3357d36
Logan Markewich <logan.markewich@live.com>
2025-05-26 12:01:41 -0600

    add openai to basic demo

--- templates/basic/src/workflow.py
+++ templates/basic/src/workflow.py
@@ -1,16 +1,19 @@
 import asyncio
 
 from llama_index.core.workflow import Workflow, StartEvent, StopEvent, step
-
+from llama_index.llms.openai import OpenAI
 
 # create a dummy workflow
 class EchoWorkflow(Workflow):
     """A dummy workflow with only one step sending back the input given."""
 
+    llm: OpenAI = OpenAI(model="gpt-4.1-nano")
+
     @step()
     async def run_step(self, ev: StartEvent) -> StopEvent:
         message = str(ev.get("message", ""))
-        return StopEvent(result=f"Message received: {message}")
+        response = await self.llm.acomplete(message)
+        return StopEvent(result=response.text)
 
 
 workflow = EchoWorkflow()
